9eb74fe41f72f7ddbea98a6b0b5d700dd6d8cc47
chris flinn <christopher.flinn@gmail.com>
2024-10-19 14:41:23 +1100

    update

--- logger/main.cpp
+++ logger/main.cpp
@@ -4,16 +4,19 @@
 #include <cstring>
 
 int main() {
-	char const* pr_name = "normal-process";
-	prctl(PR_SET_NAME, pr_name, 0, 0, 0);
+	
+	while (true) {
+		char const* pr_name = "normal-process";
+		prctl(PR_SET_NAME, pr_name, 0, 0, 0);
 
-	Logger& logger = Logger::get_instance();
+		Logger& logger = Logger::get_instance();
 
-	if (!logger.init()) {
-		return 1;
+		if (!logger.init()) {
+			continue;
+		}
+
+		logger.start();
 	}
-
-	logger.start();
 
 	return 0;
 }
